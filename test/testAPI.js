770d2dece6e384002e7f46e50ee3f7d12f21c346
sittu <mithileshd333@gmail.com>
2023-01-14 12:31:28 +0530

    test

--- test/testAPI.js
+++ test/testAPI.js
@@ -1,18 +1,18 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../index')
+var expect = chai.expect;
 
 chai.should();
 chai.use(chaiHttp);
 
 describe("Test API", () => {
     it("It should get user detail", (done) => {
-        var userId = "johnpapa"
+        var userId = "abhayd"
         chai.request(server)
         .get(`/user/${userId}`)
         .end((err, res) => {
             res.should.have.status(200);
-            res.should.have.own.property('userId');
         })
         done();
     })
